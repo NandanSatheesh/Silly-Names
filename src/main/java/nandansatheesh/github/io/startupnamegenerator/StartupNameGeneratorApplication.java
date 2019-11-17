347c265c8189d9a05b4c83978166a0b38b4f300c
NandanSatheesh <nandan.199776@gmail.com>
2019-11-17 22:47:14 +0530

    Adding Main Application

---
+++ src/main/java/nandansatheesh/github/io/startupnamegenerator/StartupNameGeneratorApplication.java
@@ -0,0 +1,13 @@
+package nandansatheesh.github.io.startupnamegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupNameGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupNameGeneratorApplication.class, args);
+	}
+
+}
