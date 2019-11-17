9d8585846fe508796db4794730b923cfce7890f5
NandanSatheesh <nandan.199776@gmail.com>
2019-11-17 22:46:05 +0530

    Base Project Setup

---
+++ src/test/java/nandansatheesh/github/io/startupnamegenerator/StartupnamegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package nandansatheesh.github.io.startupnamegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupnamegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
